f15c4ec282c1e1c045c559761211beb7e618ce54
zoccalucas <lucaszoccasoares@gmail.com>
2023-07-04 16:00:34 -0300

    feat: add form to create new support

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,3 @@
 <h1> Listagens dos suportes </h1>
+
+<a href="{{ route('supports.create')}}">Criar dúvida</a>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
 Route::get('/contact', [SiteController::class, 'contact']);
 
