7c80532ad3ded8851e65d28599c3da489e9d33c2
zoccalucas <lucaszoccasoares@gmail.com>
2023-07-05 22:03:58 -0300

    feat: add crud for supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -15,13 +15,60 @@ class SupportController extends Controller
         return view('admin/supports/index', compact('supports'));
     }
 
+    public function show(string|int $id, Support $support)
+    {
+        if (!($support = $support->find($id))) {
+            return back();
+        }
+
+        return view('admin/supports/show', compact('support'));
+    }
+
     public function create()
     {
         return view('admin/supports/create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request, Support $support)
     {
-        dd($request->all());
+        $data = $request->all();
+        $data['status'] = 'a';
+
+        $support->create($data);
+
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(string|int $id, Support $support)
+    {
+        if (!($support = $support->where('id', $id)->first())) {
+            return back();
+        }
+
+        return view('admin/supports/edit', compact('support'));
+    }
+
+    public function update(string|int $id, Support $support, Request $request)
+    {
+        if (!($support = $support->find($id))) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id, Support $support)
+    {
+        if (!($support = $support->find($id))) {
+            return back();
+        }
+
+        $support->delete();
+
+        return redirect()->route('supports.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
